4560814f47c8da234a204ac06261e4b87f5466dd
M Diaz Ardiansyah <diaz.ardian.14@gmail.com>
2026-07-03 23:35:33 +0700

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,95 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi & MQTT Configuration
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+const char* mqttServer = "test.mosquitto.org";
+const int mqttPort = 1883;
+const char* topic = "smartfarm_068/data";
+
+// MQTT Client
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Connect WiFi
+void setupWiFi() {
+
+  Serial.print("Connecting to WiFi");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi Connected!");
+}
+
+// Connect MQTT
+void reconnectMQTT() {
+
+  while (!client.connected()) {
+
+    Serial.print("Connecting to MQTT... ");
+
+    if (client.connect("ESP32Farm")) {
+
+      Serial.println("SUCCESS!");
+
+    } else {
+
+      Serial.print("FAILED, rc=");
+      Serial.print(client.state());
+      Serial.println(" -> Retry in 2 seconds");
+
+      delay(2000);
+    }
+  }
+}
+
+// Setup
+void setup() {
+
+  Serial.begin(115200);
+
+  delay(1000);
+
+  setupWiFi();
+
+  client.setServer(mqttServer, mqttPort);
+}
+
+// Loop
+void loop() {
+
+  if (!client.connected()) {
+    reconnectMQTT();
+  }
+
+  client.loop();
+
+  int grid = random(1, 501);
+
+  float temp = random(200, 380) / 10.0;
+  int hum = random(40, 90);
+  int soil = random(300, 900);
+  float ph = random(50, 75) / 10.0;
+  int light = random(200, 1000);
+
+  String payload = "{";
+  payload += "\"grid\":\"Grid" + String(grid) + "\",";
+  payload += "\"temperature\":" + String(temp) + ",";
+  payload += "\"humidity\":" + String(hum) + ",";
+  payload += "\"soil\":" + String(soil) + ",";
+  payload += "\"ph\":" + String(ph) + ",";
+  payload += "\"light\":" + String(light);
+  payload += "}";
+
+  Serial.println(payload);
+
+  client.publish(topic, payload.c_str());
+
+  delay(1000);
+}
